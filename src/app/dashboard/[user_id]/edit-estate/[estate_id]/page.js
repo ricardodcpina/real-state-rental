2315b81f8f94dfa1fd8b23a70364effd2c26b6cd
ricardo_pina <ricardodcpina@gmail.com>
2023-12-21 17:06:25 -0300

    new: added fetchEstate and updateEstate functions

--- src/app/dashboard/[user_id]/edit-estate/[estate_id]/page.js
+++ src/app/dashboard/[user_id]/edit-estate/[estate_id]/page.js
@@ -1,11 +1,28 @@
-export default function Page({params}) {
-    const { user_id, estate_id } = params
-    console.log(user_id, estate_id)
+import { cookies } from "next/headers"
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
 
-    async function getHouse(userId, houseId, formData) {
+export default async function Page({ params }) {
+    const token = cookies().get('user_token').value
+    const { user_id, estate_id } = params
+
+    async function fetchEstate() {
         'use server'
 
-        await fetch(`http://localhost:8000/houses/${houseId}`, {
+        const data = await fetch(`http://localhost:8000/houses/${estate_id}`, {
+            method: 'GET',
+            headers: { 'Authorization': `Bearer ${token}` }
+        })
+
+        const estate = await data.json()
+        return estate
+    }
+    const estate = await fetchEstate()
+
+    async function updateEstate(formData) {
+        'use server'
+
+        await fetch(`http://localhost:8000/houses/${estate_id}`, {
             method: 'PUT',
             headers: { 'Authorization': `Bearer ${token}` },
             body: formData,
@@ -13,28 +30,29 @@ export default function Page({params}) {
         })
 
         revalidatePath(`/dashboard/${user_id}`)
+        redirect(`/dashboard/${user_id}`)
     }
 
     return (
         <div className="container m-8 mx-16 w-[400px] p-4 h-1/3 bg-gradient-to-r from-zinc-300 to-zinc-200 rounded-lg font-bold text-slate-950">
-            <form className="flex flex-col" action="">
+            <form className="flex flex-col" action={updateEstate}>
                 <h1 className="text-2xl mb-4">Edit Estate</h1>
                 <label htmlFor="estate-picture">Picture</label>
-                <input id="estate-picture" name="estate-picture" type="file" accept=".jpg, .jpeg, .png" className="mb-4 " />
+                <input className="mb-4" id="estate-picture" name="thumbnail" type="file" accept=".jpg, .jpeg, .png" />
                 <label htmlFor="estate-name">Estate Name</label>
-                <input id="estate-name" name="estate-name" type="text" className="mb-4" />
+                <input className="mb-4" id="estate-name" name="description" type="text" placeholder={estate.description} />
                 <label htmlFor="estate-location">Location</label>
-                <input id="estate-location" name="estate-location" type="text" className="mb-4" />
+                <input className="mb-4" id="estate-location" name="location" type="text" placeholder={estate.location} />
                 <label htmlFor="estate-price">Price in USD</label>
-                <input id="estate-price" name="estate-price" type="number" step="100.00" className="mb-4" />
+                <input className="mb-4" id="estate-price" name="price" type="number" step="100.00" placeholder={estate.price} />
                 <div>
                     <h1>Estate available for rental?</h1>
                     <label htmlFor="estate-available">
-                        <input id="estate-available" name="estate-available" type="radio" className="mt-4 mr-1" />
+                        <input className="mt-4 mr-1" id="estate-available" name="available" type="radio" value="true" defaultChecked={estate.available === true} />
                         Yes
                     </label>
                     <label htmlFor="estate-unavailable" className="ml-2">
-                        <input id="estate-unavailable" name="estate-unavailable" type="radio" className="mr-1" />
+                        <input id="estate-unavailable" name="available" type="radio" value="false" className="mr-1" defaultChecked={estate.available === false} />
                         No
                     </label>
                 </div>

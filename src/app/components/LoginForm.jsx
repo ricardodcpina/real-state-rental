78531c2cff483c58f3a78be0ce0c423302a529e0
ricardo_pina <ricardodcpina@gmail.com>
2023-12-26 20:01:33 -0300

    new: added useFormState logic to login page | moved server actions to action.js file

--- src/app/components/LoginForm.jsx
+++ src/app/components/LoginForm.jsx
@@ -1,13 +1,22 @@
 'use client'
 
 import { useState } from "react"
+import { useFormState } from 'react-dom'
+import { loginUser, createUser } from "../lib/actions"
 
-export default function LoginForm({ loginUser, createUser }) {
+export default function LoginForm() {
+
+    const initialState = {
+        message: null
+    }
+
     const [hasAccount, setHasAccount] = useState(true)
+    const [loginState, loginUserAction] = useFormState(loginUser, initialState)
+    const [createUserState, createUserAction] = useFormState(createUser, initialState)
 
     return (
         <div className="my-8 mx-16 w-[400px] p-4 h-1/3 bg-gradient-to-r from-zinc-300 to-zinc-200 rounded-lg font-bold text-slate-950">
-            <form className="flex flex-col" action={hasAccount ? loginUser : createUser}>
+            <form className="flex flex-col" action={hasAccount ? loginUserAction : createUserAction}>
                 <div className="flex flex-grow justify-between items-baseline">
                     <h1 className="text-2xl mb-4">{hasAccount ? 'Login' : 'Register Account'}</h1>
                     <input type="button" onClick={() => setHasAccount(!hasAccount)} className="p-2 mr-2 bg-slate-500 hover:bg-slate-400 cursor-pointer rounded" value={hasAccount ? "Sign Up" : "Login"} />
@@ -28,8 +37,10 @@ export default function LoginForm({ loginUser, createUser }) {
                         <input id="confirm-password" name="confirm-password" type="password" className="mb-4" />
                     </>
                 }
+                {hasAccount && loginState.message && <h1 className="text-red-600 mb-3">{loginState.message}</h1>}
+                {!hasAccount && createUserState.message && <h1 className="text-red-600 mb-3">{createUserState.message}</h1>}
                 <div className="flex justify-end">
-                    <input type="button" className="p-2 mr-2 bg-slate-500 hover:bg-slate-400 cursor-pointer rounded" value="Cancel" />
+                    <input type="reset" className="p-2 mr-2 bg-slate-500 hover:bg-slate-400 cursor-pointer rounded" value="Cancel" />
                     <input type="submit" className="p-2 bg-slate-500 hover:bg-slate-400 cursor-pointer rounded" value="Submit" />
                 </div>
             </form>
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,48 +1,7 @@
 import LoginForm from "../components/LoginForm"
-import { redirect } from "next/navigation"
-import { cookies } from 'next/headers'
-
-
-async function loginUser(formData) {
-    'use server'
-
-    const data = await fetch('http://localhost:8000/users/auth', {
-        method: 'POST',
-        headers: { 'Content-Type': "application/json" },
-        body: JSON.stringify({
-            username: formData.get('username'),
-            password: formData.get('password'),
-        }),
-        cache: 'no-store'
-    })
-
-    const credentials = await data.json()
-
-    cookies().set('user_token', credentials.token)
-    cookies().set('user_id', credentials.userId)
-    console.log(cookies().getAll())
-    redirect(`/dashboard/${credentials.userId}`)
-}
-
-async function createUser(formData) {
-    'use server'
-
-    const createUser = await fetch('http://localhost:8000/users', {
-        method: 'POST',
-        headers: { 'Content-Type': "application/json" },
-        body: JSON.stringify({
-            username: formData.get('username'),
-            email: formData.get('email'),
-            password: formData.get('password'),
-        })
-    })
-
-    await createUser.json()
-    await loginUser(formData)
-}
 
 export default function Page() {
     return (
-        <LoginForm loginUser={loginUser} createUser={createUser} />
+        <LoginForm />
     )
 }

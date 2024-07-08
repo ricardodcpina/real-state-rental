7fc4b7b0d43c2f4808b3c7c19397b3c2a2fdf765
ricardo_pina <ricardodcpina@gmail.com>
2024-07-08 14:48:20 -0300

    removed comment

--- backend/src/services/house_service.js
+++ backend/src/services/house_service.js
@@ -64,7 +64,7 @@ exports.listHouses = async (available, limit, skip, maxCost, estateLocation, est
   return houses;
 };
 
-// MUST BE INCLUDED IN TEST
+// MUST BE INCLUDED IN TESTS
 exports.findHouse = async (houseId) => {
   // Checks Object ID validity
   if (!isValidObjectId(houseId)) throw errors.invalidID;

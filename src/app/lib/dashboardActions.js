f7c10b216e3f17a66be7d3ca7e442d0f401749d9
ricardo_pina <ricardodcpina@gmail.com>
2024-07-09 18:40:13 -0300

    try/catch blocks in dashboard actions

--- src/app/lib/dashboardActions.js
+++ src/app/lib/dashboardActions.js
@@ -1,24 +1,33 @@
 'use server';
 
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/dist/server/api-utils';
+
+const baseURL = `http://localhost:8000/dashboard`;
 
 export async function fetchMyEstates(limit = 0, skip = 0) {
+  let myEstates = [];
   const token = cookies().get('user_token')?.value;
+  const user_id = cookies().get('user_id')?.value;
 
   if (!token) redirect('/login');
 
-  const user_id = cookies().get('user_id')?.value;
+  const paginationQueryParams = `limit=${limit}&skip=${skip}`;
 
-  const data = await fetch(`http://localhost:8000/dashboard/houses?limit=${limit}&skip=${skip}`, {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  try {
+    const data = await fetch(`${baseURL}/houses?${paginationQueryParams}`, {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${token}` },
+    });
 
-  const myEstates = await data.json();
-  if (myEstates.error) {
-    return null;
+    myEstates = await data.json();
+
+    if (myEstates.error) {
+      return null;
+    }
+  } catch (error) {
+    console.log('Could not fetch owned estates');
   }
 
   revalidatePath(`/dashboard/${user_id}`);
@@ -27,19 +36,26 @@ export async function fetchMyEstates(limit = 0, skip = 0) {
 }
 
 export async function fetchMyReserves(limit = 0, skip = 0) {
+  let myReserves = [];
   const token = cookies().get('user_token')?.value;
-  if (!token) redirect('/login');
-
   const user_id = cookies().get('user_id')?.value;
 
-  const data = await fetch(`http://localhost:8000/dashboard/reserves?limit=${limit}&skip=${skip}`, {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  if (!token) redirect('/login');
 
-  const myReserves = await data.json();
-  if (myReserves.error) {
-    return null;
+  const paginationQueryParams = `limit=${limit}&skip=${skip}`;
+
+  try {
+    const data = await fetch(`${baseURL}/reserves?${paginationQueryParams}`, {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    myReserves = await data.json();
+    if (myReserves.error) {
+      return null;
+    }
+  } catch (error) {
+    console.log('Could not fetch own reserves');
   }
 
   revalidatePath(`/dashboard/${user_id}`);

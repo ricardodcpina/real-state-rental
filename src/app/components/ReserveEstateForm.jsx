8eabb74548cd80b38d97175d22c9dff46723ffe3
ricardo_pina <ricardodcpina@gmail.com>
2024-07-09 17:01:46 -0300

    migrated from useFormState to useActionState and included cancelReserve in form action

--- src/app/components/ReserveEstateForm.jsx
+++ src/app/components/ReserveEstateForm.jsx
@@ -1,15 +1,21 @@
 'use client';
 
-import { useFormState } from 'react-dom';
-import { cancelReserve, reserveEstate } from '@/app/lib/reserveActions';
 import Image from 'next/image';
+import { useActionState } from 'react';
+import { cancelReserve, reserveEstate } from '@/app/lib/reserveActions';
 
 export default function ReserveEstateForm({ estate, user_id, reserve_id }) {
   const initialState = {
     error: null,
   };
 
-  const [reserveEstateState, reserveEstateAction] = useFormState(reserveEstate, initialState);
+  const cancelReserveWithId = cancelReserve.bind(null, initialState, reserve_id);
+
+  const [reserveEstateState, reserveEstateAction] = useActionState(reserveEstate, initialState);
+  const [cancelReserveState, cancelReserveAction] = useActionState(
+    cancelReserveWithId,
+    initialState
+  );
 
   return (
     <div className='w-2/6 h-2/3 container m-16 p-4 bg-gradient-to-r from-zinc-300 to-zinc-200 rounded-lg text-slate-950 font-bold'>
@@ -35,17 +41,19 @@ export default function ReserveEstateForm({ estate, user_id, reserve_id }) {
       </div>
       <div>
         {reserve_id ? (
-          <div className='flex flex-col pt-3 items-center'>
-            <h3 className='text-lg text-green-700 font-bold'>
-              Reservation Date: {estate?.reserve?.date}
-            </h3>
-            <button
-              onClick={() => cancelReserve(reserve_id)}
-              className='mt-3 p-3 w-full bg-slate-500 hover:bg-red-800 cursor-pointer rounded-md transition-colors duration-500'
-            >
-              Cancel reserve
-            </button>
-          </div>
+          <form action={cancelReserveAction}>
+            <div className='flex flex-col pt-3 items-center'>
+              <h3 className='text-lg text-green-700 font-bold'>
+                Reservation Date: {estate?.reserve?.date}
+              </h3>
+              <button className='mt-3 mb-2 p-3 w-full bg-slate-500 hover:bg-red-800 cursor-pointer rounded-md transition-colors duration-500'>
+                Cancel reserve
+              </button>
+            </div>
+            {cancelReserveState?.error && (
+              <h1 className='text-red-600 mb-3'>{cancelReserveState.error}</h1>
+            )}
+          </form>
         ) : (
           user_id &&
           user_id !== estate.user && (
@@ -61,15 +69,15 @@ export default function ReserveEstateForm({ estate, user_id, reserve_id }) {
                   className='rounded-md text-center ml-3'
                   required
                 />
+
                 <input id='estate-id' name='estate-id' type='hidden' value={estate._id} />
-                <input id='user-id' name='user-id' type='hidden' value={user_id} />
               </div>
               <div className='flex flex-col'>
-                <button className='mt-3 p-3 bg-slate-500 hover:bg-green-800 cursor-pointer rounded-md transition-colors duration-500'>
+                <button className='mt-3 mb-2 p-3 bg-slate-500 hover:bg-green-800 cursor-pointer rounded-md transition-colors duration-500'>
                   Reserve
                 </button>
               </div>
-              {reserveEstateState.error && (
+              {reserveEstateState?.error && (
                 <h1 className='text-red-600 mb-3'>{reserveEstateState.error}</h1>
               )}
             </form>

31fed7b638387800070c64581cf64114322eba67
ricardo_pina <ricardodcpina@gmail.com>
2024-07-09 17:03:13 -0300

    try/catch for reserveActions fetch calls

--- src/app/lib/reserveActions.js
+++ src/app/lib/reserveActions.js
@@ -4,48 +4,59 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { revalidatePath } from 'next/cache';
 
+const baseURL = 'http://localhost:8000/houses';
+
 export async function reserveEstate(prevState, formData) {
   const token = cookies().get('user_token')?.value;
+  const user_id = cookies().get('user_id')?.value;
+
   if (!token) redirect('/login');
 
-  const user_id = formData.get('user-id');
   const estate_id = formData.get('estate-id');
 
-  const data = await fetch(`http://localhost:8000/houses/${estate_id}/reserves`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      date: formData.get('date'),
-    }),
-    cache: 'no-store',
-  });
+  try {
+    const data = await fetch(`${baseURL}/${estate_id}/reserves`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        date: formData.get('date'),
+      }),
+      cache: 'no-store',
+    });
 
-  const reserve = await data.json();
-  if (reserve?.error) {
-    return { error: reserve.error };
+    const reserve = await data.json();
+    if (reserve?.error) {
+      return { error: reserve.error };
+    }
+  } catch (error) {
+    console.log('Could not create reserve');
   }
 
   revalidatePath('/');
   redirect(`/dashboard/${user_id}`);
 }
 
-export async function cancelReserve(reserve_id) {
+export async function cancelReserve(prevState, reserve_id) {
   const token = cookies().get('user_token')?.value;
-  if (!token) redirect('/login');
-
   const user_id = cookies().get('user_id')?.value;
 
-  const data = await fetch(`http://localhost:8000/houses/reserves/${reserve_id}`, {
-    method: 'DELETE',
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  if (!token) redirect('/login');
 
-  const reserve = await data.json();
-  if (reserve?.error) {
-    return { error: reserve.error };
+  try {
+    const data = await fetch(`${baseURL}/reserves/${reserve_id}`, {
+      method: 'DELETE',
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const reserve = await data.json();
+    if (reserve?.error) {
+      return { error: reserve.error };
+    }
+  } catch (error) {
+    console.log('Could not cancel reserve');
   }
 
   revalidatePath(`/dashboard/${user_id}`);

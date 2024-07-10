3b422101541bd54fc06d5f50465f14a18840385f
ricardo_pina <ricardodcpina@gmail.com>
2024-07-10 14:56:37 -0300

    code formatting

--- backend/test/user_service/validate_fields.test.js
+++ backend/test/user_service/validate_fields.test.js
@@ -1,44 +1,43 @@
-const { validateFields } = require('../../src/services/user_service')
+const { validateFields } = require('../../src/utils/utils');
 
 describe('validateFields', () => {
-    describe('when the validation is successfull', () => {
-        it('returns true', () => {
-            const validate = validateFields(
-                { field_1: 'field_1', field_2: 'field_2' })
+  describe('when the validation is successfull', () => {
+    it('returns true', () => {
+      const validate = validateFields({ field_1: 'field_1', field_2: 'field_2' });
 
-            expect(validate).toBe(true)
-        })
-    })
+      expect(validate).toBe(true);
+    });
+  });
 
-    describe('when the validation fails', () => {
-        describe('when the field is undefined', () => {
-            it('returns an error', () => {
-                const input = { username: '', email: 'test@test.com' }
-                const error = { message: 'Username is required', statusCode: 400 }
+  describe('when the validation fails', () => {
+    describe('when the field is undefined', () => {
+      it('returns an error', () => {
+        const input = { username: '', email: 'test@test.com' };
+        const error = { message: 'Username is required', statusCode: 400 };
 
-                expect.assertions(1)
+        expect.assertions(1);
 
-                try {
-                    validateFields(input)
-                } catch (err) {
-                    expect(err).toEqual(error)
-                }
-            })
-        })
+        try {
+          validateFields(input);
+        } catch (err) {
+          expect(err).toEqual(error);
+        }
+      });
+    });
 
-        describe('when the field is blank', () => {
-            it('returns an error', () => {
-                const input = { username: '   ', email: 'test@test.com' }
-                const error = { message: 'Username is required', statusCode: 400 }
+    describe('when the field is blank', () => {
+      it('returns an error', () => {
+        const input = { username: '   ', email: 'test@test.com' };
+        const error = { message: 'Username is required', statusCode: 400 };
 
-                expect.assertions(1)
+        expect.assertions(1);
 
-                try {
-                    validateFields(input)
-                } catch (err) {
-                    expect(err).toEqual(error)
-                }
-            })
-        })
-    })
-})
+        try {
+          validateFields(input);
+        } catch (err) {
+          expect(err).toEqual(error);
+        }
+      });
+    });
+  });
+});
--- backend/test/user_service/verify_email.test.js
+++ backend/test/user_service/verify_email.test.js
@@ -1,55 +1,54 @@
-const realRegExpTest = RegExp.prototype.test
-const mockFind = jest.fn()
+const realRegExpTest = RegExp.prototype.test;
+const mockFind = jest.fn();
 const mockModels = {
-    User: {
-        findOne: mockFind
-    }
-}
+  User: {
+    findOne: mockFind,
+  },
+};
 
-jest.mock('../../src/models', () => mockModels)
+jest.mock('../../src/models', () => mockModels);
 
-const { verifyEmail } = require('../../src/services/user_service')
+const { verifyEmail } = require('../../src/utils/utils');
 
 describe('verifyEmail', () => {
-    describe('when the email does not exists', () => {
+  describe('when the email does not exists', () => {
+    beforeEach(() => (RegExp.prototype.test = jest.fn(() => true)));
 
-        beforeEach(() =>
-            RegExp.prototype.test = jest.fn(() => true))
+    it.only('returns true', async () => {
+      mockFind.mockResolvedValue(null);
 
-        it.only('returns true', async () => {
-            mockFind.mockResolvedValue(null)
+      const verified = verifyEmail('test@test.com');
 
-            const verified = verifyEmail('test@test.com')
+      await expect(verified).resolves.toBe(true);
+    });
 
-            await expect(verified).resolves.toBe(true)
-        })
+    afterEach(() => (RegExp.prototype.test = realRegExpTest));
+  });
 
-        afterEach(() =>
-            RegExp.prototype.test = realRegExpTest)
-    })
+  describe('when the email is not unique', () => {
+    it.only('returns an error', async () => {
+      const mockUser = jest.fn();
+      mockFind.mockResolvedValue(mockUser);
 
-    describe('when the email is not unique', () => {
-        it.only('returns an error', async () => {
-            const mockUser = jest.fn()
-            mockFind.mockResolvedValue(mockUser)
+      const verified = verifyEmail('test@test.com');
 
-            const verified = verifyEmail('test@test.com')
+      await expect(verified).rejects.toEqual({
+        message: 'EMAIL not available',
+        statusCode: 409,
+      });
+    });
+  });
 
-            await expect(verified).rejects.toEqual({
-                "message": 'EMAIL not available', "statusCode": 409
-            })
-        })
-    })
+  describe('when the email is not formatted correctly', () => {
+    it.only('returns an error', async () => {
+      mockFind.mockResolvedValue(null);
 
-    describe('when the email is not formatted correctly', () => {
-        it.only('returns an error', async () => {
-            mockFind.mockResolvedValue(null)
+      const verified = verifyEmail('invalid_email');
 
-            const verified = verifyEmail('invalid_email')
-
-            await expect(verified).rejects.toEqual({
-                'message': 'EMAIL format not valid', 'statusCode': 400
-            })
-        })
-    })
-})
+      await expect(verified).rejects.toEqual({
+        message: 'EMAIL format not valid',
+        statusCode: 400,
+      });
+    });
+  });
+});

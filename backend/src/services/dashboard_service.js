8153bffe2c786b62d06b7c54a92c3e3335582026
ricardo_pina <ricardodcpina@gmail.com>
2024-07-10 14:20:44 -0300

    included default parameters in dashboard service functions

--- backend/src/services/dashboard_service.js
+++ backend/src/services/dashboard_service.js
@@ -1,12 +1,12 @@
 const { House, Reserve } = require('../models');
 
-exports.listMyHouses = async (userId, limit, skip) => {
+exports.listMyHouses = async (userId, limit = 0, skip = 0) => {
   const houses = await House.find({ user: userId }, null, { limit, skip });
 
   return houses;
 };
 
-exports.listMyReserves = async (userId, limit, skip) => {
+exports.listMyReserves = async (userId, limit = 0, skip = 0) => {
   const reserves = await Reserve.find({ user: userId }, null, { limit, skip });
 
   await Reserve.populate(reserves, 'house');

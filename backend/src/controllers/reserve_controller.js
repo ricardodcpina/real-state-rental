a5c83671b5086c997eca3ce4c63b7b552c8767c1
ricardo_pina <ricardodcpina@gmail.com>
2024-07-10 14:21:25 -0300

    code formatting

--- backend/src/controllers/reserve_controller.js
+++ backend/src/controllers/reserve_controller.js
@@ -1,38 +1,36 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const reserveService = require('../services/reserve_service')
-const { authentication } = require('../middlewares')
+const reserveService = require('../services/reserve_service');
+const { authentication } = require('../middlewares');
 
 router.post('/:id/reserves', authentication, async (req, res) => {
-    const { userId } = req
-    const { date } = req.body
-    const houseId = req.params.id
+  const { userId } = req;
+  const { date } = req.body;
+  const houseId = req.params.id;
 
-    try {
-        const reserve = await reserveService.createReserve(userId, houseId, date)
+  try {
+    const reserve = await reserveService.createReserve(userId, houseId, date);
 
-        res.status(201).json(reserve)
-    }
-    catch (err) {
-        if (!err.statusCode) err.statusCode = 500
-        res.status(err.statusCode).json({ error: err.message })
-    }
-})
+    res.status(201).json(reserve);
+  } catch (err) {
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).json({ error: err.message });
+  }
+});
 
 router.delete('/reserves/:id', authentication, async (req, res) => {
-    const { userId } = req
-    const reserveId = req.params.id
+  const { userId } = req;
+  const reserveId = req.params.id;
 
-    try {
-        const reserve = await reserveService.cancelReserve(userId, reserveId)
+  try {
+    const reserve = await reserveService.cancelReserve(userId, reserveId);
 
-        res.status(200).json(reserve)
-    }
-    catch (err) {
-        if (!err.statusCode) err.statusCode = 500
-        res.status(err.statusCode).json({ error: err.message })
-    }
-})
+    res.status(200).json(reserve);
+  } catch (err) {
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).json({ error: err.message });
+  }
+});
 
-module.exports = router
+module.exports = router;

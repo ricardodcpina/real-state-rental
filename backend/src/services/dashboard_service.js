731f9a582d0f28893c2d915b0ad46a52322f0e83
ricardo_pina <ricardodcpina@gmail.com>
2024-07-12 15:55:39 -0300

    removed deleteMyEstates and deleteMyReserves functions

--- backend/src/services/dashboard_service.js
+++ backend/src/services/dashboard_service.js
@@ -14,21 +14,3 @@ exports.listMyReserves = async (userId, limit = 0, skip = 0) => {
   return reserves;
 };
 
-exports.deleteMyHouses = async (userId) => {
-  // Cancels all associated reserves with houses being deleted
-  // Criar campo de locador em Reserve
-
-  const deletedHouses = await House.deleteMany({ user: userId });
-
-  // Removes user images folder
-
-  return deletedHouses;
-};
-
-exports.deleteMyReserves = async (userId) => {
-  const deletedReserves = await Reserve.deleteMany({ user: userId });
-
-  // updates all houses to be available again
-
-  return deletedReserves;
-};

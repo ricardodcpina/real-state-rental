fd5f925df7117a5338e5f652c9b399683b81f096
ricardo_pina <ricardodcpina@gmail.com>
2024-07-13 09:40:31 -0300

    added getSession function and removed 'user_id' cookie

--- src/app/(pages)/estate/[estate_id]/page.js
+++ src/app/(pages)/estate/[estate_id]/page.js
@@ -1,17 +1,18 @@
-import { cookies } from 'next/headers';
 import { notFound } from 'next/navigation';
 import { fetchEstate } from '@/app/lib/estateActions';
+import { getSession } from '@/app/lib/sessionActions';
 
 import ReserveEstateForm from '@/app/components/ReserveEstateForm';
 
 export default async function EstatePage({ params }) {
   const { estate_id } = params;
-  const loggedUserId = cookies().get('user_id')?.value;
+
+  const session = await getSession();
   const estate = await fetchEstate(estate_id);
 
   if (estate?.error) {
     notFound();
   }
 
-  return <ReserveEstateForm estate={estate} user_id={loggedUserId} />;
+  return <ReserveEstateForm estate={estate} user_id={session?.sub} />;
 }
--- src/app/(pages)/estate/[estate_id]/reserve/[reserve_id]/page.js
+++ src/app/(pages)/estate/[estate_id]/reserve/[reserve_id]/page.js
@@ -1,16 +1,18 @@
-import { fetchEstate } from '@/app/lib/estateActions';
-import { cookies } from 'next/headers';
-import ReserveEstateForm from '@/app/components/ReserveEstateForm';
 import { notFound } from 'next/navigation';
+import ReserveEstateForm from '@/app/components/ReserveEstateForm';
+import { fetchEstate } from '@/app/lib/estateActions';
+import { getSession } from '@/app/lib/sessionActions';
 
 export default async function ReservePage({ params }) {
   const { estate_id, reserve_id } = params;
-  const loggedUserId = cookies().get('user_id')?.value;
+
+  const session = await getSession();
+
   const estate = await fetchEstate(estate_id);
 
-  if (estate?.reserve?._id !== reserve_id || estate?.reserve?.user !== loggedUserId) {
+  if (estate?.reserve?._id !== reserve_id || estate?.reserve?.user !== session?.sub) {
     notFound();
   }
 
-  return <ReserveEstateForm estate={estate} user_id={loggedUserId} reserve_id={reserve_id} />;
+  return <ReserveEstateForm estate={estate} user_id={session?.sub} reserve_id={reserve_id} />;
 }

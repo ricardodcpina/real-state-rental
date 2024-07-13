690e43240a4f715a22349404fc1504e9ce44ebc0
ricardo_pina <ricardodcpina@gmail.com>
2024-07-13 09:41:59 -0300

    fixed notFound with user Id | added getSession function

--- src/app/(pages)/dashboard/[user_id]/edit-estate/[estate_id]/page.js
+++ src/app/(pages)/dashboard/[user_id]/edit-estate/[estate_id]/page.js
@@ -1,14 +1,18 @@
 import UpdateEstateForm from '@/app/components/UpdateEstateForm';
 import { fetchEstate } from '@/app/lib/estateActions';
+import { getSession } from '@/app/lib/sessionActions';
 import { notFound } from 'next/navigation';
 
 export default async function EditStatePage({ params }) {
   const { estate_id, user_id } = params;
-  const estate = await fetchEstate(estate_id);
 
-  if (estate.user !== user_id) {
+  const session = await getSession();
+
+  if (session?.sub !== user_id) {
     notFound();
   }
+
+  const estate = await fetchEstate(estate_id);
 
   return <UpdateEstateForm estate={estate} />;
 }

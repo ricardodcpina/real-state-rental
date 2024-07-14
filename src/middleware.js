9d3722845f2cce6be32f2af3a89fe2e1eea02bb0
ricardo_pina <ricardodcpina@gmail.com>
2024-07-14 18:29:25 -0300

    test

--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,48 @@
+// import { NextResponse } from 'next/server';
+// import { getSession } from './app/lib/sessionActions';
+
+// export async function middleware(req) {
+//   const pathname = req.nextUrl.pathname;
+//   const isProtectedRoute =
+//     pathname.startsWith('/dashboard') || pathname.match(/\/estate\/.*\/reserve\/.*/);
+
+//   console.log('middleware running on: ' + pathname);
+
+//   const session = await getSession();
+
+//   if (!session && isProtectedRoute) {
+//     console.log('redirecting to login');
+//     const url = new URL('/login', req.url);
+//     url.searchParams.set('callbackUrl', pathname);
+//     return NextResponse.redirect(url);
+//   }
+
+//   console.log('normal redirect');
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|logo|$|login).*)'],
+// };
+
 import { NextResponse } from 'next/server';
 import { getSession } from './app/lib/sessionActions';
 
 export async function middleware(req) {
   const pathname = req.nextUrl.pathname;
 
+  console.log('middleware running on: ' + pathname);
+
   const session = await getSession();
 
   if (!session && (pathname.startsWith('/dashboard') || pathname.startsWith('/estate'))) {
+    console.log('redirecting to login');
     const url = new URL('/login', req.url);
     url.searchParams.set('callbackUrl', pathname);
     return NextResponse.redirect(url);
   }
 
+  console.log('normal redirect');
   return NextResponse.next();
 }
 
@@ -21,3 +52,5 @@ export const config = {
     '/estate/:estate_id*/reserve(.*)',
   ],
 };
+
+console.log('test')

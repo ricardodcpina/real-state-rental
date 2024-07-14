ec38cabe58fedf0f88b7c252c435a26585ad6110
ricardo_pina <ricardodcpina@gmail.com>
2024-07-13 21:14:58 -0300

    included errorHandler middleware in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const userController = require('./controllers/user_controller')
-const houseController = require('./controllers/house_controller')
-const reserveController = require('./controllers/reserve_controller')
-const dashboardController = require('./controllers/dashboard_controller')
+const userController = require('./controllers/user_controller');
+const houseController = require('./controllers/house_controller');
+const reserveController = require('./controllers/reserve_controller');
+const dashboardController = require('./controllers/dashboard_controller');
 
-app.use('/', express.json())
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
-app.use('/users', userController)
-app.use('/houses', reserveController, houseController)
-app.use('/dashboard', dashboardController)
+const { errorHandler } = require('./middlewares');
 
-exports.app = app
+app.use('/', express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use('/users', userController);
+app.use('/houses', reserveController, houseController);
+app.use('/dashboard', dashboardController);
+app.use(errorHandler);
+
+exports.app = app;

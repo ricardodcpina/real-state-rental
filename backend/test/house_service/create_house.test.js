b7d4993e8a22be6487969bdf3d5533a43bc7af01
ricardo_pina <ricardodcpina@gmail.com>
2024-07-18 09:54:23 -0300

    added validation fail test with image file in createHouse test

--- backend/test/house_service/create_house.test.js
+++ backend/test/house_service/create_house.test.js
@@ -1,32 +1,61 @@
-const mockCreate = jest.fn()
+const mockCreate = jest.fn();
 
 const mockModels = {
-    House: {
-        create: mockCreate
-    }
-}
+  House: {
+    create: mockCreate,
+  },
+};
 
-jest.mock('../../src/models', () => mockModels)
+jest.mock('../../src/models', () => mockModels);
 
-const houseService = require('../../src/services/house_service')
+const houseService = require('../../src/services/house_service');
 
 describe('createHouse', () => {
-    describe('when the validation succeeds', () => {
-        it('creates a house', async () => {
-            const mockHouse = jest.fn()
-            mockCreate.mockResolvedValue(mockHouse)
+  describe('when the validation succeeds', () => {
+    describe('when user provides a valid image file', () => {
+      it('creates a house', async () => {
+        const mockHouse = jest.fn();
+        mockCreate.mockResolvedValue(mockHouse);
 
-            const house = houseService.createHouse(
-                'testUserId', 'filename', 'description', 'location', 500, true
-            )
+        const house = houseService.createHouse(
+          'testUserId',
+          'filename',
+          'description',
+          'location',
+          500,
+          true
+        );
 
-            await expect(house).resolves.toBe(mockHouse)
+        await expect(house).resolves.toBe(mockHouse);
 
-            expect(mockCreate).toHaveBeenCalledWith({
-                user: 'testUserId', description: 'description',
-                thumbnail: 'filename', price: 500,
-                location: 'location', available: true
-            })
-        })
-    })
-})
+        expect(mockCreate).toHaveBeenCalledWith({
+          user: 'testUserId',
+          description: 'description',
+          thumbnail: 'filename',
+          price: 500,
+          location: 'location',
+          available: true,
+        });
+      });
+    });
+  });
+  describe('when the validation fails', () => {
+    describe('when user doesnt provide valid image file', () => {
+      it('returns an error', async () => {
+        const house = houseService.createHouse(
+          'testUserId',
+          undefined,
+          'description',
+          'location',
+          500,
+          true
+        );
+
+        await expect(house).rejects.toEqual({
+          message: 'Must provide an image file',
+          statusCode: 400,
+        });
+      });
+    });
+  });
+});
